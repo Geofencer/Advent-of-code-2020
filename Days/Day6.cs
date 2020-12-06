a3195880e1b266c2231983c98c0d6d4599017876
Jan van Linge <59770651+Geofencer@users.noreply.github.com>
2020-12-06 14:41:34 +0100

    Refactor day 6 to mirror Roay and Jori style :p

--- Days/Day6.cs
+++ Days/Day6.cs
@@ -5,37 +5,34 @@ namespace AdventOfCode2020
 {
     internal static class Day6
     {
-        internal static string Part1()
+        internal static string Part1() // Onliner Roy-T style :p
         {
-            // Read Inputs
-            var Questions = Input.ReadAsText("Day6");
-
-            //Solve Puzzle
-            var UniqueAnswers = Questions
+            var UniqueAnswers = Input.ReadAsText("Day6")
                 .Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
-                .Select(x => x.Replace("\n", "").Replace("\r", "").ToCharArray().Distinct().Count()).Sum();
+                .Select(x => x.Replace("\n", "")
+                .Replace("\r", "")
+                .ToCharArray()
+                .Distinct()
+                .Count())
+                .Sum()
+                .ToString();
 
             return "Unique answers: " + UniqueAnswers.ToString();
         }
 
 
-        internal static string Part2()
+        internal static string Part2() //Onliner Jori Huisman style :p
         {
-            // Read Inputs
-            var Questionaires = Input.ReadAsText("Day6");
+            var AllPositive = (from Questionaire in Input.ReadAsText("Day6").Split(Environment.NewLine + Environment.NewLine)
+                               let Respondents = Questionaire.Split(Environment.NewLine).Count()
+                               let AllAnswers = Questionaire.Replace(Environment.NewLine, "")
+                               let Answers = Questionaire.Replace(Environment.NewLine, "").ToCharArray().Distinct()
+                               select (from Answer in Answers
+                                       where Respondents == AllAnswers.Where(x => (x == Answer)).Count()
+                                       select Answer).Count()).Sum();
 
-            //Solve Puzzle
-            var AllPositive = from Questionaire in Questionaires.Split(Environment.NewLine + Environment.NewLine)
-                              let Respondents = Questionaire.Split(Environment.NewLine).Count()
-                              let AllAnswers = Questionaire.Replace(Environment.NewLine, "")
-                              let Answers = Questionaire.Replace(Environment.NewLine, "").ToCharArray().Distinct()
-                              select (from Answer in Answers
-                                      where Respondents == AllAnswers.Where(x => (x == Answer)).Count()
-                                      select Answer).Count();
 
-            var AllPositiveSum = AllPositive.Sum();
-
-            return "Questions everyone answered positive: " + AllPositiveSum;
+            return "Questions everyone answered positive: " + AllPositive;
         }
     }
 }
